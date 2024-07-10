130a0b4d650bc97dd3447d8ef032ae332d0b5dfc
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-07-10 11:27:24 -0400

    Updated ItemBehavior with reference to Gamemanager

--- Assets/Scripts/ItemBehavior.cs
+++ Assets/Scripts/ItemBehavior.cs
@@ -4,34 +4,27 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameBehavior gameManager;
+
+    private void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // 1
     // When an object collides with this item OnCollisionEnter() is called
     // This method has a param that stores a reference to the collider that ran into it
     private void OnCollisionEnter(Collision collision)
     {
-        // 2
         // gameObject holds a reference to the colliding GameObject's Collider
         // We check if that object is the player object
         if ( collision.gameObject.name == "Player")
         {
-            // 3
             // Call the Destroy method to remove the object from the scene
             Destroy(this.transform.gameObject);
-            // 4
             // Debug output that the item has been collected
             Debug.Log("Item collected!");
+
+            gameManager.Items++;
         }
     }
 }
