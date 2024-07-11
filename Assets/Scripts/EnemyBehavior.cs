f05517facd8160e359415d5bbce2489db133e019
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-07-11 16:46:37 -0400

    Added initialization logic for patrol points

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform patrolRoute;
+    public List<Transform> locations;
+
+    private void Start()
     {
-        
+        InitializePatrolRoute();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    // 1
     // OnTriggerEnter() is called whenever an object enters this object's Sphere Collider radius
     // It stores a reference to the object's Collider component
     void OnTriggerEnter(Collider other)
     {
-        // 2
+
         // We check if the Collider object is the player object
         if ( other.name == "Player")
         {
@@ -28,15 +24,24 @@ public class EnemyBehavior : MonoBehaviour
         }
     }
 
-    // 3
+
     // OnTriggerExit() is called whenever an object leaves this object's Sphere Collider radius
     void OnTriggerExit(Collider other)
     {
-        // 4
+
         // We check if the Collider object is the player object
         if (other.name == "Player")
         {
             Debug.Log("Player out of range, resume patrol");
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        // Populate locations list with Transform objects
+        foreach(Transform child in patrolRoute)
+        {
+            locations.Add(child);
         }
     }
 }
